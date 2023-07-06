821cf93cf41df419f4fda8820d09a90a1417b039
Jennifer Smith <oxyjen127@gmail.com>
2023-07-06 13:29:14 -0400

    worked on metadata and semantic HTML

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+function Footer() { 
+    return (
+        <footer>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Menu</a></li>
+                <li><a href="">Reservations</a></li>
+                <li><a href="">Order Online</a></li>
+                <li><a href="">Log In</a></li>
+            </ul>
+            <h3>Contact Us</h3>
+            <h3>Social Media</h3>
+        </footer>
+    );
+    }
+
+    export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header() { 
+return (
+    <header>
+        <img src="../icons_assets/Logo.svg" alt="Little Lemon Logo" height="68" width="222"></img>
+    </header>
+);
+}
+
+export default Header;
